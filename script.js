f14cb1102804af17eccac982e886d25cd1b4c1af
Fabio Artuso <fabioartuso88@gmail.com>
2021-01-17 09:00:01 +0000

    added volume/playback speed/fullscreen functionalities

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+const player = document.querySelector('.player');
 const video = document.querySelector('video');
 const progressRange = document.querySelector('.progress-range');
 const progressBar = document.querySelector('.progress-bar');
@@ -9,6 +10,10 @@ const volumeBar = document.querySelector('.volume-bar');
 const currentTime = document.querySelector('.time-elapsed');
 const duration = document.querySelector('.time-duration');
 const fullscreenBtn = document.querySelector('.fullscreen');
+const speed = document.querySelector('.player-speed');
+
+// Update bars' width
+const updateBarWidth = (bar, value) => bar.style.width = `${value * 100}%`;
 
 // Play & Pause ----------------------------------- //
 
@@ -43,7 +48,7 @@ function displayTime(time) {
 
 // Update Progress Bar as video plays
 function updateProgress() {
-    progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`;
+    updateBarWidth(progressBar, video.currentTime / video.duration);
     currentTime.textContent = `${displayTime(video.currentTime)} / `;
     duration.textContent = displayTime(video.duration);
 }
@@ -51,20 +56,102 @@ function updateProgress() {
 // Click to seek within the video
 function setProgress(e) {
     const newTime = e.offsetX / progressRange.offsetWidth; // point clicked on the progress bar divided the whole length of the bar
-    progressBar.style.width = `${newTime * 100}%`;
+    updateBarWidth(progressBar, newTime);
     video.currentTime = newTime * video.duration;
 }
 
 // Volume Controls --------------------------- //
 
+let lastVolume = 1;
 
+// Volume Bar
+function changeVolume(e) {
+    let volume = e.offsetX / volumeRange.offsetWidth;
+    // Rounding volume up or down
+    if (volume < 0.1) {
+        volume = 0;
+    } else if (volume > 0.9) {
+        volume = 1;
+    }
+    updateBarWidth(volumeBar, volume);
+    video.volume = volume;
+    // Change icon depending on volume
+    volumeIcon.className = '';  //start resetting icon's style
+    if (volume > 0.7) {
+        volumeIcon.classList.add('fas', 'fa-volume-up');
+    } else if (volume <= 0.7 && volume > 0) {
+        volumeIcon.classList.add('fas', 'fa-volume-down');
+    } else if (volume === 0){
+        volumeIcon.classList.add('fas', 'fa-volume-off');
+    }
+    lastVolume = volume;
+}
+
+// Mute/Unmute
+function toggleMute() {
+    volumeIcon.className = '';
+    if (video.volume) {                 //if volume > 0
+        lastVolume = video.volume;
+        video.volume = 0;
+        volumeBar.style.width = 0;
+        volumeIcon.classList.add('fas', 'fa-volume-mute');
+        volumeIcon.setAttribute('title', 'Unmute');
+    } else {                            //if volume = 0
+        video.volume = lastVolume;
+        updateBarWidth(volumeBar, lastVolume);
+        volumeIcon.classList.add('fas', (lastVolume <= 0.7) ? 'fa-volume-down' : 'fa-volume-up');
+        volumeIcon.setAttribute('title', 'Mute');
+    }
+}
 
 // Change Playback Speed -------------------- //
-
+function changeSpeed() {
+    video.playbackRate = speed.value;
+}
 
 
 // Fullscreen ------------------------------- //
 
+/* View in fullscreen */
+function openFullscreen() {
+    if (player.requestFullscreen) {
+      player.requestFullscreen();
+    } else if (player.webkitRequestFullscreen) { /* Safari */
+      player.webkitRequestFullscreen();
+    } else if (player.msRequestFullscreen) { /* IE11 */
+      player.msRequestFullscreen();
+    }
+    video.classList.add('video-fullscreen');
+  }
+  
+/* Close fullscreen */
+function closeFullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) { /* Safari */
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE11 */
+        document.msExitFullscreen();
+    }
+    video.classList.remove('video-fullscreen');
+}
+
+let fullscreen = false;
+
+// toggle fullscreen
+function toggleFullscreen() {
+    !fullscreen ? openFullscreen() : closeFullscreen();
+    fullscreen = !fullscreen;
+}
+
+// preventing the app to crush if exiting fullscreen mode through the Escape button
+// Added a fullscreenchange event listener. Acting on the element only when it exits the fullscreen mode
+function exitFullscreenEscape(){
+    if (!document.fullscreenElement) {              // if the element is exiting the fullscreen mode
+        video.classList.remove('video-fullscreen');
+        fullscreen = false;
+    }
+}
 
 
 // Event Listeners
@@ -73,3 +160,8 @@ video.addEventListener('click', togglePlay);
 video.addEventListener('timeupdate', updateProgress);
 video.addEventListener('canplay', updateProgress);
 progressRange.addEventListener('click', setProgress);
+volumeRange.addEventListener('click', changeVolume);
+volumeIcon.addEventListener('click', toggleMute);
+speed.addEventListener('change', changeSpeed);
+fullscreenBtn.addEventListener('click', toggleFullscreen);
+document.addEventListener('fullscreenchange', exitFullscreenEscape);
